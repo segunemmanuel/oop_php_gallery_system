50917ca136a6e15c773279769ea92754a5c2cf51
segun <segunemmanuel46@gmail.com>
2021-05-31 16:44:34 -0700

    new_confige.pgp,init.php
    Setting up connections
    Initializing files
    Creating the class
    Creating helper methods

---
+++ admin/includes/database.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once("new_config.php");
+
+class Database{
+
+
+    public $connection;
+
+    function __construct()
+    {
+        $this->open_db_conn();
+    }
+
+    // OPEN DB CONNECION
+    public function open_db_conn(){
+        // $this->connection=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+
+        $this->connection=new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+
+        if($this->connection->errno){
+            die("Failed".$this->connection->connect_error);
+        }
+       
+
+
+}    
+
+// DATABSE QUERY
+public function query($sql){
+
+$result=$this->connection->query($sql);
+$this->confirm_query($result);
+return $result;
+
+}
+
+// CONFIRM DB QUERY;
+
+private function confirm_query($result){
+    if(!$result){
+        die("Failed". $this->connection->connect_error);
+    }
+ 
+}
+
+
+// DB ESCAPE STRING
+public  function escape_string($string){
+$esacped_string=$this->connection->real_escape_string($string);
+return $esacped_string;
+}
+
+
+public function the_insert_id(){
+    return $this->connection->insert_id;
+}
+
+}
+
+
+ 
+
+$database=new Database;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ admin/includes/new_config.php
@@ -0,0 +1,15 @@
+<?php
+
+// Database constant connections
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_NAME','photos');
+
+
+
+
+
+
+?>
